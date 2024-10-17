951ed9dd5eb25da76b84779f4e48806cc5acbfae
Alex Simko <asimko5312@conestogac.on.ca>
2024-10-17 12:41:50 -0400

    Fixed whisper endpoint bug

--- client.py
+++ client.py
@@ -352,10 +352,10 @@ def send_audio_to_server():
             # Check for SSL and self-signed certificate settings
             if str(app_settings.SSL_ENABLE) == "1" and str(app_settings.SSL_SELFCERT) == "1":
                 # Send the request without verifying the SSL certificate
-                response = requests.post(WHISPERAUDIO_ENDPOINT, headers=headers, files=files, verify=False)
+                response = requests.post(app_settings.WHISPERAUDIO_ENDPOINT, headers=headers, files=files, verify=False)
             else:
                 # Send the request with the audio file and headers/authorization
-                response = requests.post(WHISPERAUDIO_ENDPOINT,headers=headers, files=files)
+                response = requests.post(app_settings.WHISPERAUDIO_ENDPOINT,headers=headers, files=files)
             
             # On successful response (status code 200)
             if response.status_code == 200:
