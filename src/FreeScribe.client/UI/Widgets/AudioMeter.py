fd5243e058f8210c0e6e0a6485b8d7b74e6efeee
Alex Simko <asimko5312@conestogac.on.ca>
2024-10-22 13:38:18 -0400

    Header comment

--- src/FreeScribe.client/UI/Widgets/AudioMeter.py
+++ src/FreeScribe.client/UI/Widgets/AudioMeter.py
@@ -1,3 +1,16 @@
+"""
+This software is released under the AGPL-3.0 license
+Copyright (c) 2023-2024 Braedon Hendy
+
+Further updates and packaging added in 2024 through the ClinicianFOCUS initiative, 
+a collaboration with Dr. Braedon Hendy and Conestoga College Institute of Applied 
+Learning and Technology as part of the CNERG+ applied research project, 
+Unburdening Primary Healthcare: An Open-Source AI Clinician Partner Platform". 
+Prof. Michael Yingbull (PI), Dr. Braedon Hendy (Partner), 
+and Research Students - Software Developer Alex Simko, Pemba Sherpa (F24), and Naitik Patel.
+
+"""
+
 import struct
 import tkinter as tk
 from tkinter import ttk
