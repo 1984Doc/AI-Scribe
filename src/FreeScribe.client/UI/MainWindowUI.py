28244f2010a4ba1c1375f4f8a3ff107674a3af8d
Pemba Sherpa <norsangpemba@gmail.com>
2024-10-28 15:11:08 -0400

    open markdown file under help

--- src/FreeScribe.client/UI/MainWindowUI.py
+++ src/FreeScribe.client/UI/MainWindowUI.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Toplevel
 import UI.MainWindow as mw
 import Tooltip as tt
+import markdown as md
+from tkinterhtml import HtmlFrame
 
 class MainWindowUI:
     """
@@ -105,18 +107,40 @@ class MainWindowUI:
         Create a menu bar with a Help menu.
         This method sets up the menu bar at the top of the main window and adds a Help menu with an About option.
         """
-        menu_bar = tk.Menu(self.root)
-        self.root.config(menu=menu_bar)
+        self.menu_bar = tk.Menu(self.root)
+        self.root.config(menu=self.menu_bar)
+        self._create_settings_menu()
+        self._create_help_menu()
 
+    def _create_settings_menu(self):
+        # Add Settings menu
+        self.menu_bar.add_command(label="Settings", command=self._open_settings_window)
+
+    def _create_help_menu(self):
         # Add Help menu
-        help_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=self._show_help_about)
+        help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command=lambda: self._show_help_content('src/FreeScribe.client/markdown/help/about.md'))
 
-    def _show_help_about(self):
+    
+    def _show_help_content(self, file_path):
         """
         Private method to display help/about information.
         Display help information in a message box.
         This method shows a message box with information about the application when the About option is selected from the Help menu.
         """
-        messagebox.showinfo("Help", "This is the help information for the application.")
+        help_window = Toplevel(self.root)
+        help_window.title("About")
+
+        with open(file_path, "r") as file:
+            readme = file.read()
+        
+        # Convert Markdown to HTML
+        html_content = md.markdown(readme)
+
+        # Display the HTML content in a new window using tkinterhtml
+        html_frame = HtmlFrame(help_window, horizontal_scrollbar="auto")
+        html_frame.set_content(html_content)
+        html_frame.pack(fill="both", expand=True)
+
+
