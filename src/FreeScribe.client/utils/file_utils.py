feca07414c8f0c0489e105cc589adbf1128b1222
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2024-11-12 16:23:28 -0500

    get app data dir path

--- src/FreeScribe.client/utils/file_utils.py
+++ src/FreeScribe.client/utils/file_utils.py
@@ -3,23 +3,26 @@ import os
 import sys
 
 def get_file_path(*file_names: str) -> str:
-  """
-  Get the full path to a files. Use Temporary directory at runtime for bundled apps, otherwise use the current working directory.
+    """
+    Get the full path to a files. Use Temporary directory at runtime for bundled apps, otherwise use the current working directory.
 
-  :param file_names: The names of the directories and the file.
-  :type file_names: str
-  :return: The full path to the file.
-  :rtype: str
-  """
-  base = _get_base_path(use_programdata=False)
-  return os.path.join(base, *file_names)  
+    :param file_names: The names of the directories and the file.
+    :type file_names: str
+    :return: The full path to the file.
+    :rtype: str
+    """
+    base = _get_base_path(use_programdata=False)
+    return os.path.join(base, *file_names)  
 
 def _get_programdata_path() -> str:
     """
-    Get the path to the ProgramData directory using ctypes.
+    Get the path to the AppData directory using ctypes.
+
+    :return: The path to the AppData directory.
+    :rtype: str
     """
     buf = ctypes.create_unicode_buffer(1024)
-    ctypes.windll.shell32.SHGetFolderPathW(None, 0x23, None, 0, buf)
+    ctypes.windll.shell32.SHGetFolderPathW(None, 0x001a, None, 0, buf)
     return buf.value
 
 def _get_base_path(use_programdata: bool) -> str:
