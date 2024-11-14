92d792d2cae3352609419bdbf7fccead948eac1f
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2024-11-14 11:52:57 -0500

    add flag to remove old content from dist while building

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ MAIN_SCRIPT="src/FreeScribe.client/client.py"
 ICON_PATH="src/FreeScribe.client/assets/logo.ico"
 
 # Run PyInstaller to create the standalone executable
-pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" --noconsole "$MAIN_SCRIPT"
+pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --noconsole --noconfirm
 
 # Print a message indicating that the build is complete
 echo "Build complete. Executable created: dist/$EXECUTABLE_NAME"
