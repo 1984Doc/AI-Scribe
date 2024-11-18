8d76106ec49575e8b7448833cbb24f4201e026c8
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2024-11-18 14:53:11 -0500

    set dist path when building using pyinstaller

--- build-mac.sh
+++ build-mac.sh
@@ -7,9 +7,10 @@ set -e
 EXECUTABLE_NAME="freescribe-client"
 MAIN_SCRIPT="src/FreeScribe.client/client.py"
 ICON_PATH="src/FreeScribe.client/assets/logo.ico"
+DIST_PATH="dist/freescribe-client"
 
 # Run PyInstaller to create the standalone executable
-pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --onefile --windowed
+pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" --distpath "$DIST_PATH" "$MAIN_SCRIPT" --onefile --windowed
 
 # Print a message indicating that the build is complete
 echo "Build complete. Executable created: dist/$EXECUTABLE_NAME"
--- build-pkg.sh
+++ build-pkg.sh
@@ -3,10 +3,6 @@
 # Ensure the script exits if any command fails
 set -e
 
-# Create freescribe-client directory and move the app into it
-mkdir dist/freescribe-client
-mv dist/freescribe-client.app dist/freescribe-client/
-
 # Build pkg installer for macOS using the pkgbuild command
 pkgbuild --root ./dist/freescribe-client --identifier com.clinicianfocus.freescribe --version 1.0 --install-location /Applications ./dist/installer.pkg
 
