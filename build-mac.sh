6caa1bd522ac3700ce113d57b63dfe31046bc336
Pemba Sherpa <norsangpemba@gmail.com>
2024-11-20 09:23:25 -0800

    remove --onefile flag

--- build-mac.sh
+++ build-mac.sh
@@ -9,7 +9,7 @@ MAIN_SCRIPT="src/FreeScribe.client/client.py"
 ICON_PATH="src/FreeScribe.client/assets/logo.ico"
 
 # Run PyInstaller to create the standalone executable
-pyinstaller --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --onefile --windowed
+pyinstaller --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --windowed
 
 # Print a message indicating that the build is complete
 echo "Build complete. Executable created: $DIST_PATH/$EXECUTABLE_NAME.app"
