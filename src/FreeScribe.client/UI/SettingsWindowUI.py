bf86451ad7492aeb57582b57dc12ad4ae3fb2591
Alex Simko <asimko5312@conestogac.on.ca>
2024-11-29 15:57:38 -0500

    typo fix

--- src/FreeScribe.client/UI/SettingsWindowUI.py
+++ src/FreeScribe.client/UI/SettingsWindowUI.py
@@ -209,8 +209,8 @@ class SettingsWindowUI:
             self.widgets[setting].config(state=state)
         
         # set the local option to disabled on switch to remote
-        inverted _state = "disabled" if current_state == 0 else "normal"
-        self.whisper_models_drop_down.config(state=inveresd_state)
+        inverted_state = "disabled" if current_state == 0 else "normal"
+        self.whisper_models_drop_down.config(state=inverted_state)
 
 
 
